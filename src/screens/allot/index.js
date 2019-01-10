7f7d31a9f9168117f40637f753d1145a99ba3573
Sahil Kalra <sahil.kalra@akqa.com>
2019-01-10 15:57:57 +0530

    Added text boxes and buttons in atoms

--- src/screens/allot/index.js
+++ src/screens/allot/index.js
@@ -2,12 +2,19 @@
 
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Colors } from 'constants/colors'
 import user from 'dataSet/user'
+import Button from 'components/atoms/buttons'
+import Textbox from 'components/atoms/textbox'
+import FlatButton from 'components/atoms/flat-button'
 
-const Screen1 = (props: Props) => {
+const Allot = (props: Props) => {
   return (
     <View style={styles.block}>
       <Text style={styles.empName}>{user.AKQA0065.name}</Text>
+      <Button style={{backgroundColor: Colors.blue.ribbon}} text={'Button'} width={100} onPress={() => alert()}/>
+      <Textbox placeholder={'placeholder'} />
+      <FlatButton textStyle={{color: Colors.blue.ribbon}} text={'flat btn'} onPress={() => alert()} />
     </View>
   )
 }
@@ -22,4 +29,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 })
-export default Screen1
+export default Allot
