fd9583df02122439dc56201bf74a915352aecc53
Sahil Kalra <sahil.kalra@akqa.com>
2019-01-10 13:07:59 +0530

    Added Tab Navigation, Added pulne animation

---
+++ src/navigation/index.js
@@ -0,0 +1,21 @@
+import { createStackNavigator,createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';
+
+import Allot from '../screens/allot'
+import Locate from '../screens/locate'
+import Release from '../screens/release'
+
+const AppNavigator = createMaterialTopTabNavigator({
+  Allot: { screen: Allot, navigationOptions: { title: 'Allot' }, },
+  Locate: { screen: Locate, navigationOptions: { title: 'Locate' }, },
+  Release: { screen: Release, navigationOptions: { title: 'Release' }, }
+},{
+    animationEnabled: true,
+    initialRouteName: 'Locate',
+    tabBarPosition: 'bottom',
+    swipeEnabled: true,
+    lazy: true,
+  },);
+
+const Navigator = createAppContainer(AppNavigator);
+
+export default Navigator;
